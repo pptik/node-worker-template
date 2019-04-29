6dc8589f56f0759a937d66db84a3bc20a3d8cf92
Mohamad Radisha <pr0ph0z23@gmail.com>
2019-04-29 12:03:40 +0700

    Modifikasi bin/www agar tidak listen port

--- services/publisher.js
+++ services/publisher.js
@@ -1,4 +1,4 @@
-exports.publish = async () => {
+exports.publish = async (channel) => {
   channel.publish('queue_name', new Buffer(data), {}) // queue-based
   channel.publish('exchange_name', 'routing_key', new Buffer(data, {})) // routing_key-based
 }
